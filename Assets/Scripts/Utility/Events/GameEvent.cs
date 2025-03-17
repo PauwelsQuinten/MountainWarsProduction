6d5b12557157310af57668a8186a40814559babe
PauwelsQuinten <pauwels.quinten2.0@gmail.com>
2025-03-17 15:39:07 +0100

    made the project and set it up

---
+++ Assets/Scripts/Utility/Events/GameEvent.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameEvent : ScriptableObject
+{
+    private Dictionary<GameEventListener, int> _listeners = new Dictionary<GameEventListener, int>();
+
+    public void Raise(Component sender = null, object data = null)
+    {
+        foreach (GameEventListener listener in _listeners.Keys)
+        {
+            listener.OnEventRaised(sender, data, _listeners[listener]);
+        }
+    }
+
+    public void RegisterListener(GameEventListener listener, int index)
+    {
+        _listeners.Add(listener, index);
+    }
+    public void RemoveListener(GameEventListener listener)
+    {
+        _listeners.Remove(listener);
+    }
+}
