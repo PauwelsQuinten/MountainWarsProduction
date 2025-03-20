2a24fdcd237d92414a1d8073b1adcd365864523b
Xander Geldof <xander.geldof@student.howest.be>
2025-03-20 15:31:37 +0100

    Random Push

--- Assets/Scripts/Sound/FMODAudioHandler.cs
+++ Assets/Scripts/Sound/FMODAudioHandler.cs
@@ -1,40 +1,35 @@
 using FMOD.Studio;
 using UnityEngine;
 using FMODUnity;
+
 public class FMODAudioHandler : MonoBehaviour
 {
-    public static FMODAudioHandler Instance { get; private set; }
-    
     private EventInstance _footstepsSFXInstance;
     private PARAMETER_ID _surfaceTypeID;
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(this.gameObject);
-        }
-        else
-        {
-            Instance = this;
-        }
-    }
 
-    //Global Parameters
+    // Global Parameters
 
-    //Events
+    // Events
     [SerializeField] private EventReference _footstepsSFX;
     public EventReference FootstepsSFX => _footstepsSFX;
-    
+
     [SerializeField] private EventReference _attackChargeSFX;
     public EventReference AttackChargeSFX => _attackChargeSFX;
-    
+
     [SerializeField] private EventReference _swordSwooshSFX;
     public EventReference SwordSwooshSFX => _swordSwooshSFX;
-    
+
     [SerializeField] private EventReference _swordHitSFX;
     public EventReference SwordHitSFX => _swordHitSFX;
+
     
-    public static void GetParameterID(EventInstance eventInstance, string parameterName, out PARAMETER_ID parameterID)
+    public void TestFunction(Component sender, object obj)
+    {
+        
+    }
+    
+    
+    private void GetParameterID(EventInstance eventInstance, string parameterName, out PARAMETER_ID parameterID)
     {
         // Get the parameter ID
         eventInstance.getDescription(out EventDescription eventDescription);
@@ -42,9 +37,10 @@ public class FMODAudioHandler : MonoBehaviour
         parameterID = parameterDescription.id;
     }
 
-    public static void SetParameterByID(EventInstance eventInstance, PARAMETER_ID parameterID, float desiredParameterValue)
+    private void SetParameterByID(EventInstance eventInstance, PARAMETER_ID parameterID, float desiredParameterValue)
     {
         // Set the parameter value by ID
         eventInstance.setParameterByID(parameterID, desiredParameterValue);
     }
+
 }
