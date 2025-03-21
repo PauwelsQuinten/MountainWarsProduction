8f8c5dd4cc40716950310450c8ed71c88b39504d
Xander Geldof <xander.geldof@student.howest.be>
2025-03-21 16:54:41 +0100

    Updated FMOD Audio Handler
    Updated the audio handler so it takes more parameters. It also more methods now to keep it more organized

--- Assets/Scripts/Sound/FMODAudioHandler.cs
+++ Assets/Scripts/Sound/FMODAudioHandler.cs
@@ -1,42 +1,34 @@
 using FMOD.Studio;
 using UnityEngine;
 using FMODUnity;
+using UnityEngine.Serialization;
 
 public class FMODAudioHandler : MonoBehaviour
 {
     private AimingOutputArgs _aimingEventArgs;
-
+    private AttackEventArgs _attackEventArgs;
+    //Parameters
+    //Local
     private PARAMETER_ID _surfaceTypeID;
+    private float _surfaceTypeIDValue;
+    private PARAMETER_ID _weaponHitSurfaceID;
+    private float _weaponHitSurfaceIDValue;
     
+    //Global
     private PARAMETER_ID _attacksStrengthID;
     private float _attacksStrengthIDValue;
-
-    private PARAMETER_ID _swordHitSurfaceID;
-    private float _swordHitSurfaceIDValue;
-
-
-    private float volume;
+    private PARAMETER_ID _currentWeaponID;
+    private float _currentWeaponIDValue;
+    
     // Events
     [SerializeField] private EventReference _footstepsSFX;
     private EventInstance _footstepsSFXInstance;
     [SerializeField] private EventReference _attackChargeSFX;
     private EventInstance _attackChargeSFXInstance;
-    [SerializeField] private EventReference _swordSwooshSFX;
-    private EventInstance _swordSwooshSFXInstance;
-    [SerializeField] private EventReference _swordHitSFX;
-    private EventInstance _swordHitSFXInstance;
-
-    private void Start()
-    {
-        // Get the parameter ID for SurfaceType
-        GetParameterID(_footstepsSFXInstance, "SurfaceType", out _surfaceTypeID);
-
-        // Get the parameter ID for SwordHitSurface
-        
-        // Get the parameter ID for AttacksStrength
-        RuntimeManager.StudioSystem.getParameterDescriptionByName("AttackStrength", out PARAMETER_DESCRIPTION attacksStrengthDescription);
-        _attacksStrengthID = attacksStrengthDescription.id;
-    }
+   [SerializeField] private EventReference _weaponSwooshSFX;
+    private EventInstance _weaponSwooshSFXInstance;
+    [SerializeField] private EventReference _weaponHitSFX;
+    private EventInstance _weaponHitSFXInstance;
 
     private void GetParameterID(EventInstance eventInstance, string parameterName, out PARAMETER_ID parameterID)
     {
@@ -51,8 +43,14 @@ public class FMODAudioHandler : MonoBehaviour
         // Set the parameter value by ID
         eventInstance.setParameterByID(parameterID, desiredParameterValue);
     }
-
-    private void SetGlobalParameterByID(PARAMETER_ID parameterID, float desiredParameterValue)
+    private void GetGlobalParameterID( string parameterName, out PARAMETER_ID parameterID)
+    {
+        // Get the global parameter value by ID
+        RuntimeManager.StudioSystem.getParameterDescriptionByName(parameterName, out PARAMETER_DESCRIPTION parameterDescription);
+        parameterID = parameterDescription.id;
+    }
+    
+    private void SetGlobalParameterID(PARAMETER_ID parameterID, float desiredParameterValue)
     {
         // Set the global parameter value by ID
         RuntimeManager.StudioSystem.setParameterByID(parameterID, desiredParameterValue);
@@ -63,33 +61,49 @@ public class FMODAudioHandler : MonoBehaviour
         _footstepsSFXInstance = RuntimeManager.CreateInstance(_footstepsSFX);
         _footstepsSFXInstance.start();
         _footstepsSFXInstance.release();
-
     }
 
-    public void PlaySwordHitSFX(Component sender, object obj)
+    public void PlayWeaponHitSFX(Component sender, object obj)
     {
-        _swordHitSFXInstance = RuntimeManager.CreateInstance(_swordHitSFX);
-        GetParameterID(_swordHitSFXInstance, "SwordHitSurface", out _swordHitSurfaceID);
+        _weaponHitSFXInstance = RuntimeManager.CreateInstance(_weaponHitSFX);
+        GetParameterID(_weaponHitSFXInstance, "WeaponSurfaceHit", out _weaponHitSurfaceID);
+        GetGlobalParameterID("AttacksStrength", out _attacksStrengthID);
+        GetGlobalParameterID("CurrentWeapon", out _currentWeaponID);
+        if (_attackEventArgs == null)
+        {
+            _attackEventArgs = obj as AttackEventArgs;
+        }
         if (_aimingEventArgs == null)
         {
             _aimingEventArgs = obj as AimingOutputArgs;
         }
-        SetGlobalParameterByID(_attacksStrengthID, _aimingEventArgs.Speed);
-        SetParameterByID(_swordHitSFXInstance, _swordHitSurfaceID, _swordHitSurfaceIDValue);
-        _swordHitSFXInstance.start();
-        _swordSwooshSFXInstance.release();
+        SetGlobalParameterID(_attacksStrengthID, _attackEventArgs.AttackPower);
+        SetGlobalParameterID(_currentWeaponID, 2.0f);
+        SetParameterByID(_weaponHitSFXInstance, _weaponHitSurfaceID, _weaponHitSurfaceIDValue);
+        _weaponHitSFXInstance.start();
+        _weaponSwooshSFXInstance.release();
     }
 
-    public void PlaySwordSwooshSFX(Component sender, object obj)
+    public void PlayWooshSFX(Component sender, object obj)
     {
-        _swordSwooshSFXInstance = RuntimeManager.CreateInstance(_swordSwooshSFX);
+        _weaponSwooshSFXInstance = RuntimeManager.CreateInstance(_weaponSwooshSFX);
+        GetGlobalParameterID("AttackStrength", out _attacksStrengthID);
+        GetGlobalParameterID("CurrentWeapon", out _currentWeaponID);
+        if (_attackEventArgs == null)
+        {
+            _attackEventArgs = obj as AttackEventArgs;
+        }
         if (_aimingEventArgs == null)
         {
             _aimingEventArgs = obj as AimingOutputArgs;
         }
-        SetGlobalParameterByID(_attacksStrengthID, _aimingEventArgs.Speed);
 
-        _swordSwooshSFXInstance.start();
-        _swordSwooshSFXInstance.release();
+        if (_aimingEventArgs.AttackSignal != AttackSignal.Idle)
+        {
+            SetGlobalParameterID(_attacksStrengthID, _aimingEventArgs.Speed);
+            SetGlobalParameterID(_currentWeaponID, 1.0f);
+            _weaponSwooshSFXInstance.start();
+            _weaponSwooshSFXInstance.release();
+        }
     }
 }
