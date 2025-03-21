d1603b0d5b9c94cd211a16fcdae7fd9e210da929
TGoncpp <141250817+TGoncpp@users.noreply.github.com>
2025-03-21 09:51:30 +0100

    set events

---
+++ Assets/Scripts/BaseCharacter/Blocking.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Blocking : MonoBehaviour
+{
+    public void BlockMovement(Component sender, object obj)
+    {
+
+    }
+    public void CheckBlock(Component sender, object obj)
+    {
+
+    }
+
+}
