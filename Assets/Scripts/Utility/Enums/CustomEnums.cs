94c89d68f6aa424efdc169e36f252fa6ba97eae0
PauwelsQuinten <pauwels.quinten2.0@gmail.com>
2025-03-21 16:52:48 +0100

    Changed the aiming and attack to use charged power

--- Assets/Scripts/Utility/Enums/CustomEnums.cs
+++ Assets/Scripts/Utility/Enums/CustomEnums.cs
@@ -25,7 +25,7 @@ namespace UnityEngine
         Attack,
         ShieldDefence,
         SwordDefence,
-        BlockAttack
+        LockShield
     }
 
     public enum CurrentWeapon
@@ -53,9 +53,9 @@ namespace UnityEngine
 
     public enum AttackSignal
     {
-        Idle, 
-        Stab, 
-        Feint, 
+        Idle,
+        Stab,
+        Feint,
         Swing,
         Charge
     }
